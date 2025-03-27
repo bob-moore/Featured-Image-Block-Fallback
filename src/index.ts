ca270fa7a1f27797d6ccc861c3276502f04d1d9e
Bob Moore <bobjutsu@gmail.com>
2025-03-27 16:48:59 -0500

    Beta release

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Wordpress dependencies
- */
-import { addFilter } from '@wordpress/hooks';
-import { Edit } from './Edit.tsx';
-
-addFilter('editor.BlockEdit', 'marked-effect/with-fallback-image-edit', Edit );
