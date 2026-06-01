d9cc5af5e9bf07c63b78c89158d3983e0b698af1
Bob Moore <bobjutsu@gmail.com>
2026-06-01 08:38:05 -0500

    Release 0.3.4

--- scoper.custom.php
+++ scoper.custom.php
@@ -1,28 +1,27 @@
 <?php
 
 function customize_php_scoper_config( array $config = [] ): array {
-	$config['exclude-namespaces'] = array_merge(
-		$config['exclude-namespaces'] ?? [],
-		[ 'Composer' ]
-	);
 
 	$config['patchers'] = array_merge(
 		$config['patchers'] ?? [],
 		[
 			function ( string $filePath, string $prefix, string $content ): string {
-				if ( strpos( $filePath, 'ObjectCreationCompiler.php' ) === false ) {
+				if ( strpos( $filePath, '/Compiler/' ) === false ) {
 					return $content;
 				}
 
-				// PHP-Scoper rewrites namespace tokens but cannot see inside sprintf() string
-				// literals that emit PHP code. This literal hard-codes the ObjectCreator FQCN
-				// for the private-property-injection branch of the compiled container cache.
+				// PHP-Scoper rewrites namespace tokens but cannot see inside string literals
+				// that PHP-DI uses to emit compiled-container code.
 				return str_replace(
 					[
+						"return \\DI\\Definition\\StringDefinition::resolveExpression(",
+						"throw new \\DI\\Definition\\Exception\\InvalidDefinition(",
 						"'\\DI\\Definition\\Resolver\\ObjectCreator::",
 						"'\\\\DI\\\\Definition\\\\Resolver\\\\ObjectCreator::",
 					],
 					[
+						"return \\{$prefix}\\DI\\Definition\\StringDefinition::resolveExpression(",
+						"throw new \\{$prefix}\\DI\\Definition\\Exception\\InvalidDefinition(",
 						"'\\{$prefix}\\DI\\Definition\\Resolver\\ObjectCreator::",
 						"'\\\\" . str_replace( '\\', '\\\\', $prefix ) . "\\\\DI\\\\Definition\\\\Resolver\\\\ObjectCreator::",
 					],
